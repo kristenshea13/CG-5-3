ea399d56ecb24c8652e179db9825e1dadf9365bd
Kristen Shea <kstar61@live.com>
2018-08-08 17:56:42 -0500

    *

--- CG 5-3 Dice Game/Program.cs
+++ CG 5-3 Dice Game/Program.cs
@@ -6,7 +6,37 @@ namespace CG_5_3_Dice_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create a dice game to see who can get to 20 first.You will roll 
+            //two 6 - sided dice and add up the numbers.  Then the computer gets to 
+            //go, and it does the same.  This process will repeat until either you or 
+            //the computer wins.You should put the rolling of the dice into its own method.  
+            //Make sure your methods have XML comments for IntelliSense.
+
+            Console.WriteLine("Beat the Computer! First to 20 wins!");
+            Console.WriteLine(); //blank line between game name and first user entry line for readability
+            Console.Write("Player 1 name: ");
+            string player1 = Console.ReadLine();
+            string player2 = "Computer";
+            Console.Write($"{player1} vs {player2}");
+
+
+
+
+
+            Console.ReadLine();
+
+
         }
+
+        private static int ()
+            {
+
+            }
+
+
+
+
+
+
     }
 }
